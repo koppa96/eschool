ce9ff48d63920c34035054aa03a8448a683e04ff
Koppa Péter Kornél <koppa96@windowslive.com>
2020-12-10 02:29:38 +0100

    Create common exception handler

---
+++ ESchool.Libs.AspNetCore/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ESchool.Libs.AspNetCore.Middlewares
+{
+    public class ExceptionHandlerMiddleware 
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<ExceptionHandlerMiddleware> logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            this.next = next;
+            this.logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception e)
+            {
+                if (e is InvalidOperationException)
+                {
+                    context.Response.StatusCode = 400;
+                    await WriteJsonAsync(context.Response, new ProblemDetails
+                    {
+                        Title = "Bad Request",
+                        Status = 400,
+                        Detail = e.Message
+                    });
+                }
+
+                // TODO: Implement exception handling, create unique exception types
+            }
+        }
+
+        private Task WriteJsonAsync(HttpResponse response, object data)
+        {
+            return response.WriteAsync(JsonSerializer.Serialize(data, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            }));
+        }
+    }
+}
