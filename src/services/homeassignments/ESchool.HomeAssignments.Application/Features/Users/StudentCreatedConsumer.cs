0a823809f23d233622e67eaca71498b9d690fa54
Koppa Péter <koppa96@windowslive.com>
2021-02-20 10:33:42 +0100

    Restructure physical solution strutcture

---
+++ src/services/homeassignments/ESchool.HomeAssignments.Application/Features/Users/StudentCreatedConsumer.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ESchool.ClassRegister.Interface.IntegrationEvents.UserCreation;
+using ESchool.HomeAssignments.Domain;
+using ESchool.HomeAssignments.Domain.Entities.Users;
+using MassTransit;
+
+namespace ESchool.HomeAssignments.Application.Features.Users
+{
+    public class StudentCreatedConsumer : IConsumer<StudentCreatedIntegrationEvent>
+    {
+        private readonly HomeAssignmentsContext dbContext;
+
+        public StudentCreatedConsumer(HomeAssignmentsContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task Consume(ConsumeContext<StudentCreatedIntegrationEvent> context)
+        {
+            dbContext.Students.Add(new Student
+            {
+                Id = context.Message.Id,
+                Name = context.Message.Name,
+                UserId = context.Message.UserId,
+                TenantId = context.Message.TenantId
+            });
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
