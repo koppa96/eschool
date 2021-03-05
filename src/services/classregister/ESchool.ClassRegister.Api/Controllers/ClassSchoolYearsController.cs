36210a2eccd6b600d288ffa45ea359dc60519e05
Koppa Péter <koppa96@windowslive.com>
2021-03-05 18:36:21 +0100

    Create endpoint for managing class-schoolyear relationships

---
+++ src/services/classregister/ESchool.ClassRegister.Api/Controllers/ClassSchoolYearsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ESchool.ClassRegister.Application.Features.ClassSchoolYears;
+using ESchool.Libs.Domain.Enums;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESchool.ClassRegister.Api.Controllers
+{
+    [Authorize(nameof(TenantRoleType.Administrator))]
+    [ApiController]
+    [Route("api/school-years/{schoolYearId}/classes}")]
+    public class ClassSchoolYearsController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ClassSchoolYearsController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost("{classId}")]
+        public Task AddClassToSchoolYear(Guid schoolYearId, Guid classId, CancellationToken cancellationToken)
+        {
+            return mediator.Send(new ClassSchoolYearCreateCommand
+            {
+                ClassId = classId,
+                SchoolYearId = schoolYearId
+            }, cancellationToken);
+        }
+
+        [HttpDelete("{classId}")]
+        public Task RemoveClassFromSchoolYear(Guid schoolYearId, Guid classId, CancellationToken cancellationToken)
+        {
+            return mediator.Send(new ClassSchoolYearDeleteCommand
+            {
+                ClassId = classId,
+                SchoolYearId = schoolYearId
+            }, cancellationToken);
+        }
+    }
+}
