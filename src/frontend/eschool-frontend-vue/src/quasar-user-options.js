5afac08595ccba944d498bdb1159d622d4fe413c
Koppa Péter <koppa96@windowslive.com>
2021-09-04 21:15:09 +0200

    Add quasar component library

---
+++ src/frontend/eschool-frontend-vue/src/quasar-user-options.js
@@ -0,0 +1,14 @@
+
+import './styles/quasar.scss'
+import lang from 'quasar/lang/hu.js'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {
+  },
+  lang: lang
+}
