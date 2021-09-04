c2568b84d1dd90258138782f578b6558b4e5c4e5
Koppa Péter <koppa96@windowslive.com>
2021-09-04 15:25:05 +0200

    Vite config

---
+++ src/frontend/eschool-frontend-vue/vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import path from 'path'
+const srcPath = path.resolve(__dirname, 'src', 'styles', 'variables.scss')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  define: { 'process.env': {} },
+  css: {
+    preprocessorOptions: {
+      sass: { additionalData: `@import ${srcPath}\n` },
+      scss: { additionalData: `@import ${srcPath};\n` },
+    },
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ]
+  },
+})
