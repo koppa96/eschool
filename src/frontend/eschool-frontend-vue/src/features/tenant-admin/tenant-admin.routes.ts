359dd947139003f09246cdc04666a1b1c5699375
Koppa Péter <koppa96@windowslive.com>
2021-09-27 21:54:38 +0200

    TenantAdmin route refactor, metadata

--- src/frontend/eschool-frontend-vue/src/features/tenant-admin/tenant-admin.routes.ts
+++ src/frontend/eschool-frontend-vue/src/features/tenant-admin/tenant-admin.routes.ts
@@ -1,28 +1,63 @@
-import { RouteRecordRaw } from 'vue-router'
+import { RouteLocationNormalizedLoaded, RouteRecordRaw } from 'vue-router'
+import { isString } from 'lodash-es'
+import { createClient } from '@/shared/api'
+import { TenantsClient } from '@/shared/generated-clients/identity-provider'
 
 export const tenantAdminRoutes: RouteRecordRaw[] = [
   {
     path: '/tenants',
     component: () =>
-      import(/* webpackChunkName: "tenant-admin" */ './views/TenantsView.vue')
-  },
-  {
-    path: '/tenants/:id',
-    component: () =>
       import(
-        /* webpackChunkName: "tenant-admin" */ './views/TenantDetailsView.vue'
-      )
+        /* webpackChunkName: "tenant-admin" */ './views/TenantsLayoutView.vue'
+      ),
+    meta: {
+      name: 'Iskolák'
+    },
+    children: [
+      {
+        path: '',
+        component: () =>
+          import(
+            /* webpackChunkName: "tenant-admin" */ './views/TenantsView.vue'
+          )
+      },
+      {
+        path: ':id',
+        component: () =>
+          import(
+            /* webpackChunkName: "tenant-admin" */ './views/TenantDetailsView.vue'
+          ),
+        meta: {
+          resolveName: async (route: RouteLocationNormalizedLoaded) => {
+            const client = createClient(TenantsClient)
+
+            if (isString(route.params.id)) {
+              const tenant = await client.getTenant(route.params.id)
+              return tenant.name
+            }
+
+            return 'Iskola részletei'
+          }
+        }
+      }
+    ]
   },
   {
     path: '/users',
     component: () =>
-      import(/* webpackChunkName: "tenant-admin" */ './views/UsersView.vue')
+      import(/* webpackChunkName: "tenant-admin" */ './views/UsersView.vue'),
+    meta: {
+      name: 'Felhasználók'
+    }
   },
   {
     path: '/my-tenant',
     component: () =>
       import(
         /* webpackChunkName: "tenant-admin" */ './views/TenantDetailsView.vue'
-      )
+      ),
+    meta: {
+      name: 'Iskolám'
+    }
   }
 ]
