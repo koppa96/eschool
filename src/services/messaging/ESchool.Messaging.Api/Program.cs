bbdf9bef838c7dda5dbd1ba5e335e5ae7194d150
Koppa Péter <koppa96@windowslive.com>
2021-11-10 12:55:17 +0100

    Migrate messaging dbs on startup

--- src/services/messaging/ESchool.Messaging.Api/Program.cs
+++ src/services/messaging/ESchool.Messaging.Api/Program.cs
@@ -1,19 +1,43 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using ESchool.Libs.Domain.MultiTenancy;
+using ESchool.Messaging.Domain;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Polly;
 
 namespace ESchool.Messaging.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+            await Policy.Handle<Exception>()
+                .WaitAndRetryForeverAsync(x => TimeSpan.FromSeconds(3),
+                    (exception, span) => logger.LogCritical(exception, "Failed to start up."))
+                .ExecuteAsync(async () =>
+                {
+                    using var scope = host.Services.CreateScope();
+                    var masterDbContext = scope.ServiceProvider.GetRequiredService<MasterDbContext>();
+                    await masterDbContext.Database.MigrateAsync();
+
+                    var tenants = await masterDbContext.Tenants.ToListAsync();
+                    var factory = scope.ServiceProvider
+                        .GetRequiredService<ITenantDbContextFactory<MessagingContext>>();
+                    foreach (var tenant in tenants)
+                    {
+                        await using var tenantDbContext = factory.CreateContext(tenant);
+                        await tenantDbContext.Database.MigrateAsync();
+                    }
+                });
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
