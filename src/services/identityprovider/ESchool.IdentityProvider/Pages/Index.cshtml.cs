50a2fd435a3c6e74a83f8fceab9717af5ea3bde2
Koppa Péter <koppa96@windowslive.com>
2021-11-29 22:46:08 +0100

    Add starting page for idp

---
+++ src/services/identityprovider/ESchool.IdentityProvider/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ESchool.IdentityProvider.Pages
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
