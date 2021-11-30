57bdd72ce294ed48990a3d486e4e7857d6c96f28
Koppa Péter <koppa96@windowslive.com>
2021-11-30 22:23:49 +0100

    Add ability to generate dummy passwords

---
+++ src/services/identityprovider/ESchool.IdentityProvider.Application/Configuration/DummyPasswordConfiguration.cs
@@ -0,0 +1,8 @@
+namespace ESchool.IdentityProvider.Application.Configuration
+{
+    public class DummyPasswordConfiguration
+    {
+        public bool GenerateDummyPassword { get; set; }
+        public string DummyPassword { get; set; }
+    }
+}
--- src/services/identityprovider/ESchool.IdentityProvider.Application/Features/Users/UserCreateHandler.cs
+++ src/services/identityprovider/ESchool.IdentityProvider.Application/Features/Users/UserCreateHandler.cs
@@ -2,10 +2,12 @@
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using ESchool.IdentityProvider.Application.Configuration;
 using ESchool.IdentityProvider.Domain.Entities.Users;
 using ESchool.IdentityProvider.Interface.Features.Users;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
 
 namespace ESchool.IdentityProvider.Application.Features.Users
 {
@@ -13,13 +15,16 @@ namespace ESchool.IdentityProvider.Application.Features.Users
     {
         private readonly IMapper mapper;
         private readonly UserManager<User> userManager;
+        private readonly DummyPasswordConfiguration dummyPasswordConfiguration;
 
         public UserCreateHandler(
             UserManager<User> userManager,
-            IMapper mapper)
+            IMapper mapper,
+            IOptions<DummyPasswordConfiguration> options)
         {
             this.userManager = userManager;
             this.mapper = mapper;
+            dummyPasswordConfiguration = options.Value;
         }
 
         public async Task<UserDetailsResponse> Handle(UserCreateCommand request, CancellationToken cancellationToken)
@@ -29,10 +34,13 @@ namespace ESchool.IdentityProvider.Application.Features.Users
                 UserName = request.Email,
                 Name = request.Name,
                 Email = request.Email,
-                GlobalRole = request.GlobalRole
+                GlobalRole = request.GlobalRole,
             };
 
-            var result = await userManager.CreateAsync(user);
+            var result = dummyPasswordConfiguration.GenerateDummyPassword 
+                ? await userManager.CreateAsync(user, dummyPasswordConfiguration.DummyPassword)
+                : await userManager.CreateAsync(user);
+            
             if (result.Succeeded) return mapper.Map<UserDetailsResponse>(user);
 
             throw new InvalidOperationException("Could not create user. Email is already taken.");
